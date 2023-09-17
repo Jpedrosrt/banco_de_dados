1080a796fd26a6abab4c672231f6da15ddf9cdf6
João Pedro <j0310pedro@gmail.com>
2023-09-17 19:54:50 -0300

    consultaas

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ programa = MeuProgramaPG(
     password="postgres",
     port="5432"
 )
+    
 
 # Rota para a página inicial
 @app.route('/')
@@ -20,23 +21,21 @@ def index():
     return render_template('index.html')
 
 # Rota para a página de inserção de funcionarios
-@app.route('/inserir_funcionaro', methods=['GET', 'POST'])
-def inserir_cargo():
-    if request.method == 'POST':
-        valores = (
-            request.form['nome_do_cargo']
-        )
-        programa.inserir_linha('cargo', valores)
-        return redirect('/')
-    return render_template('inserir_cargo.html')
+@app.route('/adicionar_funcionario')
+def adicionar_funcionario():
 
-@app.route('/inserir_dados_pessoais', methods=['GET', 'POST'])
-def inserir_dados_pessoais():
+    id_funcionario = programa.adicionar_funcionario()
+    
+    if id_funcionario is not None:
+        return redirect(f'/adicionar_dados_pessoais/{id_funcionario}')
+    else:
+        return "Erro ao adicionar funcionário. Por favor, tente novamente."
+
+
+@app.route('/adicionar_dados_pessoais/<int:id_funcionario>', methods=['GET', 'POST'])
+def adicionar_dados(id_funcionario):
+
     if request.method == 'POST':
-        if request.form['dependentes_id']:
-            dependentes_id = int(request.form['dependentes_id'])
-        else:
-            dependentes_id = None
         valores = (
             request.form['cpf'],
             request.form['genero'],
@@ -44,57 +43,66 @@ def inserir_dados_pessoais():
             request.form['data_de_nascimento'],
             request.form['nome_completo'],
             request.form['email'],
-            request.form['funcionarios_id'],
-            dependentes_id
+            id_funcionario,
+            None
         )
-        programa.inserir_linha('dados_pessoais', valores)  # Não é necessário colocar RH.dados_pessoais
-        return redirect('/')
-    return render_template('inserir_dados_pessoais.html')
 
+        id_dados_pessoais = programa.adicionar_dados('dados_pessoais', valores)
+        if id_dados_pessoais is not None:
+            print(id_dados_pessoais)
+            return redirect(f'/adicionar_telefones/{id_dados_pessoais}')
+        else:
+            return "Erro ao adicionar dados. Por favor, tente novamente."
+        
+    return render_template('adicionar_dados_pessoais.html', id_funcionario=id_funcionario)
 
-# Rota para a página de inserção de cargos
-@app.route('/inserir_cargo', methods=['GET', 'POST'])
-def inserir_cargo():
+@app.route('/adicionar_telefones/<int:id_dados_pessoais>', methods=['GET', 'POST'])
+def adicionar_telefones(id_dados_pessoais):
     if request.method == 'POST':
-        valores = (
-            request.form['nome_do_cargo']
-        )
-        programa.inserir_linha('cargo', valores)
-        return redirect('/')
-    return render_template('inserir_cargo.html')
+        num_telefones = len(request.form)
+        telefones = [request.form[f'telefone{i}'] for i in range(num_telefones)]
 
-# Rota para a página de associação de funcionários a cargos
-@app.route('/associar_funcionario_cargo', methods=['GET', 'POST'])
-def associar_funcionario_cargo():
+        for telefone in telefones:
+            print(telefone)
+            id_telefone = programa.adicionar_telefone(telefone, id_dados_pessoais)
+            if id_telefone is None:
+                return "Erro ao adicionar telefone. Por favor, tente novamente."
+
+        return redirect(f'/adicionar_enderecos/{id_dados_pessoais}')
+    return render_template('adicionar_telefones.html', id_dados_pessoais=id_dados_pessoais, num_telefones=1)
+
+@app.route('/adicionar_enderecos/<int:id_dados_pessoais>', methods=['GET', 'POST'])
+def adicionar_enderecos(id_dados_pessoais):
     if request.method == 'POST':
-        valores = (
-            request.form['funcionario_id'],
-            request.form['cargo_id']
+        valores_endereco = (
+            request.form['lagradouro'],
+            request.form['cep'],
+            request.form['complemento'],
+            request.form['numero'],
+            request.form['bairro'],
+            id_dados_pessoais
         )
-        programa.inserir_linha('funcionarios_cargo', valores)
-        return redirect('/')
-    return render_template('associar_funcionario_cargo.html')
 
-# Rota para a página de consulta de funcionários
-@app.route('/consultar_funcionarios')
-def consultar_dados_pessoais():
-    resultados = programa.consultar_tabela('dados_pessoais')
-    if resultados is not None:
-        return render_template('consultar_funcionarios.html', resultados=resultados)
-    else:
-        return render_template('sem_resultados.html')
+        id_endereco = programa.adicionar_endereco(valores_endereco)
+        if id_endereco is not None:
+            programa.conn.commit()
+            return redirect('/')
+        else:
+            return "Erro ao adicionar endereço. Por favor, tente novamente."
 
-# Rota para a página de consulta de cargos
-@app.route('/consultar_cargos')
-def consultar_cargos():
-    resultados = programa.consultar_tabela('cargo')
-    return render_template('consultar_cargos.html', resultados=resultados)
+    return render_template('adicionar_enderecos.html', id_dados_pessoais=id_dados_pessoais)
 
-# Rota para a página de consulta de associações
-@app.route('/consultar_associacoes')
-def consultar_associacoes():
-    resultados = programa.consultar_tabela('funcionarios_cargo')
-    return render_template('consultar_associacoes.html', resultados=resultados)
+@app.route('/consultar_funcionarios', methods=['GET', 'POST'])
+def consultar_funcionarios():
+    if request.method == 'POST':
+        id_funcionario = request.form.get('id_funcionario')
+        funcionario = programa.consultar_funcionario(id_funcionario)
+        if funcionario is not None:
+            return render_template('mostrar_funcionario.html', funcionario=funcionario)
+        else:
+            return "Funcionário não encontrado."
+    return render_template('consultar_funcionarios.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db.py
+++ db.py
@@ -10,64 +10,104 @@ class MeuProgramaPG:
             port=port
         )
 
-    def inserir_linha(self, tabela, valores):
+    def adicionar_funcionario(self):
         cursor = self.conn.cursor()
         try:
-            placeholders = ', '.join(['%s'] * len(valores))
-
-            print(valores)
-            insert_sql = f"INSERT INTO RH.{tabela} (cpf, genero, estado_civil, data_de_nascimento, nome_completo, email, funcionarios_id, dependentes_id) VALUES ({placeholders});"
-
-            cursor.execute(insert_sql, tuple(valores))  # Convertendo de volta para uma tupla
-            self.conn.commit()
-            print(f"Linha inserida com sucesso na tabela RH.{tabela}.")
+            cursor.execute("INSERT INTO RH.funcionarios DEFAULT VALUES RETURNING id;")
+            id_funcionario = cursor.fetchone()[0]
+            return id_funcionario
         except psycopg2.Error as e:
-            self.conn.rollback()  # Tente fazer um rollback explicitamente
-            print(f"Erro ao inserir linha na tabela RH.{tabela}: {e}")
-        finally:
-            cursor.close()
-
-
-    def consultar_tabela(self, tabela):
-        cursor = self.conn.cursor()
-        try:
-            consulta_sql = f"SELECT * FROM rh.{tabela};"
-            cursor.execute(consulta_sql)
-            resultados = cursor.fetchall()
-            return resultados
-        except psycopg2.Error as e:
-            print(f"Erro ao consultar a tabela rh.{tabela}: {e}")
+            print(f"Erro ao adicionar funcionário: {e}")
+            self.conn.rollback()
             return None
         finally:
             cursor.close()
 
-    """def realizar_transacao(self, tabela):
+    def adicionar_dados(self, tabela, valores):
         cursor = self.conn.cursor()
         try:
-            # Inicie a transação
-            self.conn.begin()
+            placeholders = ', '.join(['%s'] * len(valores))
 
-            # Operação de consulta
-            consulta_sql = f"SELECT * FROM RH.{tabela};"
-            cursor.execute(consulta_sql)
-            resultados = cursor.fetchall()
+            # Operação de inserção
+            insert_sql = f"INSERT INTO RH.{tabela} (cpf, genero, estado_civil, data_de_nascimento, nome_completo, email, funcionarios_id, dependentes_id) VALUES ({placeholders}) RETURNING id;"
 
-            # Operação de inserção/atualização
-            insert_sql = f"INSERT INTO RH.{tabela} (coluna) VALUES (%s);"
-            cursor.execute(insert_sql, ('valor',))
+            cursor.execute(insert_sql, tuple(valores))
+            id_dados_pessoais = cursor.fetchone()[0]
+            print(id_dados_pessoais)
 
-            # Finalize a transação
-            self.conn.commit()
-
-            print("Transação bem-sucedida.")
-
+            return id_dados_pessoais
         except psycopg2.Error as e:
-            # Em caso de erro, faça rollback na transação
+            # Em caso de erro, desfazer a transação
             self.conn.rollback()
-            print(f"Erro durante a transação: {e}")
+            print(f"Erro ao inserir linha na tabela RH.{tabela}: {e}")
+            return None
         finally:
-            cursor.close()"""
+            cursor.close()
 
+    def adicionar_telefone(self, telefone, dados_pessoais_id):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO RH.telefone (telefone, dados_pessoais_id) VALUES (%s, %s) RETURNING id;", (telefone, dados_pessoais_id))
+            id_telefone = cursor.fetchone()[0]
+            return id_telefone
+        except psycopg2.Error as e:
+            self.conn.rollback()
+            print(f"Erro ao adicionar telefone: {e}")
+            return None
+        finally:
+            cursor.close()
+    
+    def adicionar_endereco(self, valores):
+        cursor = self.conn.cursor()
+        try:
+            placeholders = ', '.join(['%s'] * len(valores))
+
+            # Operação de inserção
+            insert_sql = f"INSERT INTO RH.endereco (lagradouro, cep, complemento, numero, bairro, dados_pessoais_id) VALUES ({placeholders}) RETURNING id;"
+            cursor.execute(insert_sql, tuple(valores))
+            id_endereco = cursor.fetchone()[0]
+
+            return id_endereco
+        except psycopg2.Error as e:
+            # Em caso de erro, desfazer a transação
+            self.conn.rollback()
+            print(f"Erro ao inserir linha na tabela RH.endereco: {e}")
+            return None
+        finally:
+            cursor.close()
+
+
+    def consultar_funcionario(self, id_funcionario):
+        cursor = self.conn.cursor()
+        try:
+            # Consulta para obter dados pessoais do funcionário
+            cursor.execute("SELECT * FROM RH.funcionarios WHERE id = %s;", (id_funcionario,))
+            funcionario = cursor.fetchone()
+
+            if funcionario:
+                # Consulta para obter dados pessoais
+                cursor.execute("SELECT * FROM RH.dados_pessoais WHERE funcionarios_id = %s;", (id_funcionario,))
+                dados_pessoais = cursor.fetchone()
+
+                # Consulta para obter telefones
+                cursor.execute("SELECT * FROM RH.telefone WHERE dados_pessoais_id = %s;", (dados_pessoais[0],))
+                telefones = cursor.fetchall()
+
+                # Consulta para obter endereço
+                cursor.execute("SELECT * FROM RH.endereco WHERE dados_pessoais_id = %s;", (dados_pessoais[0],))
+                endereco = cursor.fetchone()
+
+                # Adicionando telefones ao resultado
+                funcionario = list(funcionario) + list(dados_pessoais) + list(endereco) + telefones
+                print(funcionario)
+                return funcionario
+            else:
+                return None
+        except psycopg2.Error as e:
+            print(f"Erro ao consultar funcionário: {e}")
+            return None
+        finally:
+            cursor.close()
 
     def fechar_conexao(self):
         self.conn.close()
